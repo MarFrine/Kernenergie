a847e253bc4a61ce39168adfbab0c861398ef853
MarFrine <81744843+MarFrine@users.noreply.github.com>
2021-04-01 11:29:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+function mine(namel) {
+	alert("testhi" + namel);
+	
+}
+
+mine("zwei");
+
+function craft(nummer) {
+	return nummer * nummer;
+	
+}
+
+let nimm = craft(3);
+console.log(nimm);
+console.log(craft(5));
+
+
+
+alert("test");
+let names = "hi";
+let kast = 30;
+let kost = true;
+let color = null;
+let person = {
+	name: "jasd",
+	age: 23
+};
+person.name = "dase";
+person["name"] = "hist";
+console.log(person);
+
+//Array
+let liste = ["hi", 3, "blue"];
+console.log(liste[2]);
+console.log(liste);
